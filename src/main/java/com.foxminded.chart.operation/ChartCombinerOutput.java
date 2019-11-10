378855f5c046d56a64022c542af76945fa3995f6
evergood <kanzann@gmail.com>
2019-11-11 00:58:58 +0300

    files and classes

--- src/main/java/com.foxminded.chart.operation/ChartCombinerOutput.java
+++ src/main/java/com.foxminded.chart.operation/ChartCombinerOutput.java
@@ -1,4 +1,15 @@
 package com.foxminded.chart.operation;
 
+import java.text.ParseException;
+
 public class ChartCombinerOutput {
+    public static void main(String[] args) throws ParseException {
+        ChartLine line = new ChartLine();
+        line.setName("Igor Anolievich");
+        line.setTeam("Lada Racing Club");
+        line.setStartTime("2018-05-24_12:02:58.917");
+        line.setEndTime("2018-05-24_12:04:03.332");
+        System.out.println(line.toString());
+    }
 }
+
---
+++ src/main/java/com.foxminded.chart.operation/ChartLine.java
@@ -0,0 +1,42 @@
+package com.foxminded.chart.operation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ChartLine {
+    private String name;
+    private String team;
+    private Date startTime;
+    private Date endTime;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS");
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setStartTime(String startTime) throws ParseException {
+        this.startTime = dateFormat.parse(startTime);
+    }
+
+    public void setEndTime(String endTime) throws ParseException {
+        this.endTime = dateFormat.parse(endTime);
+    }
+
+
+    public String toString() {
+        char SEPARATOR = '|';
+        SimpleDateFormat timeFormat = new SimpleDateFormat("m:ss.SSS");
+        return new StringBuilder()
+                .append(String.format("%-20s", name))
+                .append(SEPARATOR)
+                .append(String.format("%-27s", team))
+                .append(SEPARATOR)
+                .append(timeFormat.format(endTime.getTime() - startTime.getTime()))
+                .toString();
+    }
+}
